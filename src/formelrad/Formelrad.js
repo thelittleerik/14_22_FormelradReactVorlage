5554a071792b7a7fab0736617efe272a08aff0bd
Erik Oros <erik.oros1995@gmail.com>
2025-06-12 10:46:53 +0200

    Revert "Calculation u and p is required"
    This reverts commit 8fc5add8342b74493529898a67b4506916960117.

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -7,23 +7,8 @@ export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
         i: 2,
-        r: "",
-        p: ""
+        r: ""
     })
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("handleSubmit")
-        if (values.u === "" && values.i === "") {
-            /*calculate u and i */
-            setValues(values => ({...values, u: Math.sqrt(values.p * values.r)}));
-            setValues(values => ({...values, i: Math.sqrt(values.p / values.r)}));
-        } else if (values.u === "" && values.r === "") {
-            /*calculate u and r */
-            setValues(values => ({...values, u: values.p / values.i}));
-            setValues(values => ({...values, r: values.p / values.i / values.i}));
-        }
-    }
 
     return (
         <>
@@ -32,9 +17,9 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
